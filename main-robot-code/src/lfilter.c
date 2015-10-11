cf00cab74ea47a4607aba8b4acb6bfabe1130ae3
Feilan Jiang <feilanjiang@gmail.com>
2015-10-11 00:43:22 -0400

    Add comments to global functions

--- main-robot-code/src/lfilter.c
+++ main-robot-code/src/lfilter.c
@@ -9,6 +9,15 @@ int chindex[MOTOR_LIMIT] = { [0 ... MOTOR_LIMIT - 1] = -1 };	// ideal type: int8
 int fcycles[MOTOR_LIMIT] = { 0 };								// ideal type: uint8_t
 int count = 0;													// ideal type: uint8_t
 
+/**
+ * Initializes the linear filter for the specified motor channel. A motor channel cannot be
+ * initialized more than once.
+ *
+ * Parameters:
+ * channel - the motor channel to be initialized
+ * num_fcycles - the duration of the filtering effect; a higher value will result in a more
+ * 				 gradual acceleration
+ */
 void lfilterInit(const int/*uint8_t*/ channel, int/*uint8_t*/ num_fcycles) {
 	if (channel > 0 && channel <= MOTOR_LIMIT
 			&& count < MOTOR_LIMIT
@@ -24,6 +33,25 @@ void lfilterInit(const int/*uint8_t*/ channel, int/*uint8_t*/ num_fcycles) {
 	}
 }
 
+/**
+ * Takes in a speed value for a specified channel and calculates a filtered speed. If the
+ * channel hasn't been initialized, a speed of 0 will be returned.
+ *
+ * Because linear filters are meant to provide gradual acceleration, the filtered speed will
+ * most likely be different from the target speed specified by the speed parameter.
+ *
+ * A linear filter provides accleration by calculating an average of the most recent speed
+ * values. Each time a new speed is given, it is added to the list of recent speed
+ * values; the oldest value on the list is then removed before a new average is calculated.
+ * For a more detailed description, see this link:
+ * http://www.vexforum.com/showpost.php?p=234355&postcount=14
+ *
+ * Parameters:
+ * channel - the channel for which to calculate a filtered speed
+ * speed - the speed value that is to be applied to the filter; must be between -127 and 127
+ *
+ * Returns: a filtered speed value between -127 and 127
+ */
 int/*int8_t*/ getfSpeed(const int/*uint8_t*/ channel, int/*int16_t*/ speed) {
 	int fspeed = 0;
 	ch = chindex[channel - 1];
@@ -46,6 +74,9 @@ int/*int8_t*/ getfSpeed(const int/*uint8_t*/ channel, int/*int16_t*/ speed) {
 	return fspeed;
 }
 
+/**
+ * Clears the linear filter for each motor channel.
+ */
 void lfilterReset(void) {
 	for (ch = 0; ch < MOTOR_LIMIT; ++ch) {
 		for (cy = 0; cy < fcycles[ch]; ++cy) {
--- main-robot-code/src/opcontrol.c
+++ main-robot-code/src/opcontrol.c
@@ -37,29 +37,46 @@
 const int DRIVE_AXIS = 3;		// ideal type: uint8_t
 const int STRAFE_AXIS = 4;		// ideal type: uint8_t
 const int ROTATION_AXIS = 1;	// ideal type: uint8_t
+
 const int JOYSTICK_SLOT = 1;	// ideal type: uint8_t
 const int ROTATION_DIVIDER = 2;	// ideal type: uint8_t
 
+/**
+ * TODO: test field-centric drive
+ * TODO: see if signs need to be changed
+ *
+ * Drives and/or rotates the robot at the specified speed. The direction is determined from the
+ * hypotenuse of vx and vy (see "Parameters" for their definitions).
+ *
+ * Because the robot has a holonomic drive, it is capable of traversing at any angle regardless
+ * of its orientation.
+ *
+ * This function only applies one pulse to the drive motors and should be called from within a
+ * loop.
+ *
+ * Parameters:
+ * vx - the horizontal speed of the robot; -127 for full left, 127 for full right
+ * vy - the vertical speed of the robot; -127 for full down, 127 for full up
+ * r  - the rotational speed of the robot; -127 for full counterclockwise, 127 for full clockwise
+ * is_field_centric - if set to true, the direction of the robot's motion will be relative to the
+ * 					  playing field rather than the robot
+ */
 void drive(int/*int8_t*/ vx, int/*int8_t*/ vy, int/*int8_t*/ rotation, bool is_field_centric) {
 	int/*int16_t*/ flspeed, blspeed, frspeed, brspeed;
 
-	if (is_field_centric) {	// TODO: test
+	if (is_field_centric) {
 		float angle = (float) gyroGet(gyro) * M_PI / 180;
 		float v = hypotf(vx, vy);
 		vx = (int) (v * sinf(angle));
 		vy = (int) (v * cosf(angle));
 	}
 
-	// TODO: test
+	// Linear filtering for gradual acceleration and reduced motor wear
 	flspeed = getfSpeed(FRONT_LEFT_MOTOR_CHANNEL, -vy - vx + rotation);
 	blspeed = getfSpeed(BACK_LEFT_MOTOR_CHANNEL, -vy + vx + rotation);
 	frspeed = getfSpeed(FRONT_RIGHT_MOTOR_CHANNEL, vy - vx + rotation);
 	brspeed = getfSpeed(BACK_RIGHT_MOTOR_CHANNEL, vy + vx + rotation);
 
-	/* assumptions: for STRAFE_AXIS, 127 = full right
-	 * 				for DRIVE_AXIS, 127 = full forward
-	 * 				for ROTATION_AXIS, 127 = full clockwise
-	 */
 	motorSet(FRONT_LEFT_MOTOR_CHANNEL, flspeed);
 	motorSet(BACK_LEFT_MOTOR_CHANNEL, blspeed);
 	motorSet(FRONT_RIGHT_MOTOR_CHANNEL, frspeed);
