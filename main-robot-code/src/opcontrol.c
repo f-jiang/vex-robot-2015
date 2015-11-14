006166250d2f23326f9fe67d22a58ee245cb6fd2
Feilan Jiang <feilanjiang@gmail.com>
2015-11-13 21:13:54 -0500

    Comment out gyro code

--- main-robot-code/src/opcontrol.c
+++ main-robot-code/src/opcontrol.c
@@ -70,10 +70,10 @@ void drive(int8_t vx, int8_t vy, int8_t r, bool is_field_centric) {
 	r /= 2;
 
 	if (is_field_centric) {
-		float angle = gyroGet(gyro) % 360 * M_PI / 180;
-		float v = hypotf(vx, vy);
-		vx = (int8_t) (v * sinf(angle));
-		vy = (int8_t) (v * cosf(angle));
+//		float angle = gyroGet(gyro) % 360 * M_PI / 180;
+//		float v = hypotf(vx, vy);
+//		vx = (int8_t) (v * sinf(angle));
+//		vy = (int8_t) (v * cosf(angle));
 	}
 
 	// Linear filtering for gradual acceleration and reduced motor wear
